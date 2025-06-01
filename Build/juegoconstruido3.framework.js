59df3eed38098cf51ad48c8a107f05d65154b0bb
RVDveloper <yorjes.valerio@estudiant.fjaverianas.com>
2025-06-01 17:07:39 +0200

    Add build WebGL with Git LFS

---
+++ Build/juegoconstruido3.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f3f09904f64757143bdcdebc44cf9e289191e1d05a90a7c604000c935ed8b7e7
+size 843109
---
+++ Build/juegoconstruido3.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9ea0d77acaa871b694a49537913f09107ea1989b9dbfe64e2152b8b42cc13cd
+size 57636
